dde7d188714c7d00d347af08cfcbb095aedc15d7
Ümit Tunç <umitreva@gmail.com>
2024-06-28 14:36:49 +0300

    key optional value same path assign name

--- src/CacheHelper.php
+++ src/CacheHelper.php
@@ -3,8 +3,12 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 
 if (!function_exists('include_cache')) {
-    function include_cache($path, $key, $minute = 60, $variables = [])
+    function include_cache($path, $key = "", $minute = 60, $variables = [])
     {
+        if($key == "") {
+            $key = $path;
+        }
+        
         $cachedView = Cache::remember($key, $minute * 60, function () use ($path, $variables) {
             return view($path, $variables)->render();
         });
